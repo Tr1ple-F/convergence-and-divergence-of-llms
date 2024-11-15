aa97900ebafc25ca7d6e343fcf394e244e28ee79
Finlay Fehlauer <finlay.fehlauer@gmail.com>
2024-11-15 20:51:00 +0100

    Updated Visualizer.ipynb

--- plotters/utils.py
+++ plotters/utils.py
@@ -10,22 +10,6 @@ def seeds_config():
     with open("./seeds_config.json") as f:
         return json.load(f)
 
-def tokenized_text():
-    with open("../input_text_tokenized_old.json") as f:
-        return json.load(f)
-
-# Function to load the appropriate .npy file
-def load_npy_file(model_name, revision, middle="_", appendix="kl"):
-    model_name_sanitized = model_name.replace('/', '-')
-    filename = f"{model_name_sanitized}{middle}{revision}{middle}{appendix}.npy"
-    filepath = os.path.join("../results/deduped/", filename)
-
-    if os.path.exists(filepath):
-        data = np.load(filepath)
-        return data
-    else:
-        raise FileNotFoundError(f"The file {filename} does not exist in the 'results' folder.")
-
 def find_comparison_index(model_name, revision):
     try:
         model_names = deduped_config()['model_names']
