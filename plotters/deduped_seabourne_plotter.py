809cacf6ea5aaf2e67a7266b8ab9d82f905cff2b
Finlay Fehlauer <finlay.fehlauer@gmail.com>
2024-11-15 21:35:52 +0100

    First basic plots

---
+++ plotters/deduped_seabourne_plotter.py
@@ -0,0 +1,53 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from utils import deduped_config
+
+config = deduped_config()
+revisions = config['revisions']
+models = config['model_names']
+number_of_steps = len(revisions)
+number_of_models = len(models)
+last_step = revisions[-1]
+
+def strip(name):
+    if "EleutherAI" in name:
+        return name.replace("EleutherAI/pythia-", "").replace("-deduped", "")
+    if "step" in name:
+        return name.replace("step", "")
+    return name
+
+# Idea 1
+# x = training step
+# y = multiple KLs, different model size
+for model in models:
+    kl_data = np.load(f'../results/deduped/{model.replace("/", "-")}-{last_step}-kl.npy')
+    selected_data = np.average(kl_data, axis=1)
+
+    data = []
+    for i, value in enumerate(selected_data):
+        data.append({'Training Step': strip(revisions[i % number_of_steps]), 'Model': strip(models[int(i / number_of_steps)]), 'KL Value': value})
+    df = pd.DataFrame(data)
+
+    sns.lineplot(x='Training Step', y='KL Value', hue='Model', data=df, estimator=None)
+
+    plt.savefig(f'../graphics/across_training_{model.replace("/", "-")}.png')
+    plt.close()
+
+# Idea 2
+# x = model_size
+# y = multiple KLs, all same model, different training steps
+for model in models:
+    kl_data = np.load(f'../results/deduped/{model.replace("/", "-")}-{last_step}-kl.npy')
+    selected_data = np.average(kl_data, axis=1)
+
+    data = []
+    for i, value in enumerate(selected_data):
+        data.append({'Training Step': strip(revisions[i % number_of_steps]), 'Model': strip(models[int(i / number_of_steps)]), 'KL Value': value})
+    df = pd.DataFrame(data)
+
+    sns.lineplot(x='Model', y='KL Value', hue='Training Step', data=df, estimator=None)
+
+    plt.savefig(f'../graphics/across_model_{model.replace("/", "-")}.png')
+    plt.close()
