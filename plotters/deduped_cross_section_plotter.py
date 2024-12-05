e4c35cff06ab5e00b6471edce3fe918798763474
Finlay Fehlauer <finlay.fehlauer@gmail.com>
2024-12-05 10:49:22 +0100

    Log steps on deduped and a bunch of plots

---
+++ plotters/deduped_cross_section_plotter.py
@@ -0,0 +1,26 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+import sys
+import json
+
+# Load the dataframe
+df = pd.read_csv(f'../working_dir/{sys.argv[1]}/output/average_dataframe.csv')
+
+from utils import deduped_config, strip
+
+config = deduped_config()
+revisions = config['revisions']
+models = config['model_names']
+
+print(df)
+print(models)
+
+for x in models:
+    df_plot = df[df['Model 1'] == strip(x)]
+    df_plot = df_plot[df_plot['Revision 1'] == df_plot['Revision 2']]
+    plt.figure(figsize=(10, 6))
+    sns.lineplot(data=df_plot, x='Revision 1', y='KL Average', hue='Model 2', markers=True)
+    plt.xscale('log')
+    plt.savefig(f'../working_dir/{sys.argv[1]}/output/deduped_no_seeds_{strip(x)}.png')
+    plt.close()
--- plotters/seeds_confidence_interval_plotter_bench.py
+++ plotters/seeds_confidence_interval_plotter_bench.py
@@ -12,20 +12,19 @@ with open(f'../common/pos_tags.json', 'r') as f:
     pos_tags = json.load(f)
 
 # value_vars = [f'KL Average - {x}' for x in ["VB", "CC", "CD", "JJ", "PRP", "RP", "RB", "WP", "DT", "IN", "MD", "NN"]]
-value_vars = [f'KL Average - {x}' for x in ["VB", "JJ", "IN", "NN"]]
+# value_vars = [f'KL Average - {x}' for x in ["VB", "JJ", "IN", "NN"]]
 # value_vars = [f'KL Average - {x["tag"]}' for x in pos_tags]
 
-for x in ['14m', '31m', '70m', '160m', '410m']:
-    df_plot = df.melt(id_vars=['Model 1', 'Model 2', 'Revision 1', 'Revision 2', 'Seed 1', 'Seed 2'], value_vars=value_vars, var_name='PoS Tag', value_name='Average KL')
-    df_plot = df_plot[df_plot['Model 1'] == x]
-    df_plot = df_plot[df_plot['Model 2'] == x]
-    df_plot = df_plot[df_plot['Revision 1'] == df_plot['Revision 2']]
-    df_plot = df_plot[df_plot['Seed 1'] != df_plot['Seed 2']]
-    plt.figure(figsize=(10, 6))
-    sns.lineplot(data=df_plot, x='Revision 1', y='Average KL', hue='PoS Tag', errorbar='sd', markers=True)
-    plt.xscale('log')
-    plt.savefig(f'../working_dir/{sys.argv[1]}/output/tiago_test_pos_tag_{x}.png')
-    plt.close()
+# for x in ['14m', '31m', '70m', '160m', '410m']:
+df_plot = df[df['Model 1'] == df['Model 2']]
+# df_plot = df_plot[df_plot['Model 2'] == x]
+df_plot = df_plot[df_plot['Revision 1'] == df_plot['Revision 2']]
+df_plot = df_plot[df_plot['Seed 1'] != df_plot['Seed 2']]
+plt.figure(figsize=(10, 6))
+sns.lineplot(data=df_plot, x='Revision 1', y='KL Average', hue='Model 1', errorbar='sd', markers=True)
+plt.xscale('log')
+plt.savefig(f'../working_dir/{sys.argv[1]}/output/tiago_test_bench_pos_tag.png')
+plt.close()
 
 
 
