9d9148073c529f28592b3ffc46cee4102ac1c73d
Finlay Fehlauer <finlay.fehlauer@gmail.com>
2024-12-07 11:42:47 +0100

    Added the distribution characteristics

---
+++ plotters/seeds_uni_plot.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import sys
+import seaborn as sns
+
+df = pd.read_csv(f'../working_dir/{sys.argv[1]}/output/seed_dist_average_dataframe.csv')
+
+plt.figure(figsize=(10, 6))
+sns.lineplot(data=df, x='Revision 1', y='Uniform KL', hue='Model 1', errorbar='sd', markers=True)
+plt.xscale('log')
+plt.savefig(f'../working_dir/{sys.argv[1]}/output/seeds_uniform.png')
+plt.close()
+
+plt.figure(figsize=(10, 6))
+sns.lineplot(data=df, x='Revision 1', y='Unigram KL', hue='Model 1', errorbar='sd', markers=True)
+plt.xscale('log')
+plt.savefig(f'../working_dir/{sys.argv[1]}/output/seeds_unigram.png')
+plt.close()
