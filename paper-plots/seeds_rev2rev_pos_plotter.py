0e5798f4642c29715dc7a7946fe1a2ac63551bdf
Tr1ple-F <32393151+Tr1ple-F@users.noreply.github.com>
2025-05-06 21:19:39 -0400

    Adding clean plots for paper submission

---
+++ paper-plots/seeds_rev2rev_pos_plotter.py
@@ -0,0 +1,68 @@
+import pandas as pd
+import sys
+from utils import styled_plot
+
+pos_index = sys.argv[3]
+
+df = pd.read_csv(f'../working_dir/{sys.argv[1]}/output/seeds_pos_{pos_index}_dataframe.csv')
+
+def plot_internal(value_vars, appendix=""):
+    for model_size in ['14m', '31m', '70m', '160m', '410m']:
+        df_plot = df.melt(
+            id_vars=['Model', 'Model 2', 'Training Step', 'Training Step 2', 'Seed 1', 'Seed 2'],
+            value_vars=value_vars,
+            var_name='PoS Tag',
+            value_name='Average KL'
+        )
+        df_plot = df_plot[
+            (df_plot['Model'] == model_size) &
+            (df_plot['Model 2'] == model_size) &
+            (df_plot['Training Step'] == df_plot['Training Step 2']) &
+            (df_plot['Seed 1'] != df_plot['Seed 2'])
+            ]
+
+        y_label = r'Expected convergence ($\mathbb{E}[\mathrm{conv}]$)'
+        save_loc = f'../working_dir/{sys.argv[1]}/output/seeds_rev2rev_model_{model_size}{appendix}_{pos_index}.png'
+
+        styled_plot(df_plot, 'Training Step', 'Average KL', 'PoS Tag', 'PoS Tag', 'Training Step', y_label, save_loc)
+
+def plot_no_filters():
+    df_plot = df.copy()
+    df_plot = df_plot[
+        (df_plot['Model'] == df_plot['Model 2']) &
+        (df_plot['Training Step'] == df_plot['Training Step 2']) &
+        (df_plot['Seed 1'] != df_plot['Seed 2'])
+        ]
+
+    y_label = r'Expected convergence ($\mathbb{E}[\mathrm{conv}]$)'
+    save_loc = f'../working_dir/{sys.argv[1]}/output/seeds_no_filters.png'
+
+    styled_plot(df_plot, 'Training Step', 'KL Average', 'Model', 'Model', 'Training Step', y_label, save_loc)
+
+# ====== Run the functions based on sys.argv[2] ======
+
+noun_vars = [f'KL Average - {x}' for x in ["NN", "NNS", "NNP"]]
+other_vars = [f'KL Average - {x}' for x in ["PRP$", "DT", "CC", "JJ", "IN", "TO", "RB", "MD", "PRP"]]
+verb_vars = [f'KL Average - {x}' for x in ["VB", "VBG", "VBN", "VBD", "VBZ"]]
+grouped_vars = [f'KL Average - {x}' for x in ["JJ", "IN", "DT", "MD", "PRP", "Nouns", "Verbs"]]
+
+if sys.argv[2] == "none":
+    plot_no_filters()
+
+if sys.argv[2] == "other":
+    plot_internal(other_vars,"_other")
+
+if sys.argv[2] == "nouns":
+    plot_internal(noun_vars, "_nouns")
+
+if sys.argv[2] == "verbs":
+    plot_internal(verb_vars, "_verbs")
+
+if sys.argv[2] == "grouped":
+    plot_internal(grouped_vars, "_grouped")
+
+if sys.argv[2] == "all":
+    plot_internal(other_vars,"_other")
+    plot_internal(noun_vars, "_nouns")
+    plot_internal(verb_vars, "_verbs")
+    plot_internal(grouped_vars, "_grouped")
---
+++ paper-plots/utils.py
@@ -0,0 +1,79 @@
+import json
+import sys
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+mpl.rcParams['font.family'] = "serif"
+
+def deduped_config():
+    with open(f'../working_dir/{sys.argv[1]}/deduped_config.json') as f:
+        return json.load(f)
+
+def seeds_config():
+    with open(f'../working_dir/{sys.argv[1]}/seeds_config.json') as f:
+        return json.load(f)
+
+def tagged_tokens():
+    with open(f'../working_dir/{sys.argv[1]}/pos_tagged_tokens.json', 'r') as f:
+        return json.load(f)
+
+def pos_tags():
+    with open('../common/pos_tags.json', 'r') as f:
+        return json.load(f)
+
+def strip(name):
+    if "EleutherAI" in name:
+        return name.replace("EleutherAI/pythia-", "").replace("-deduped", "")
+    if "step" in name:
+        return name.replace("step", "")
+    return name
+
+def styled_plot(df_plot, x_name, y_name, hue, style, x_label, y_label, save_loc):
+    plt.figure(figsize=(10, 6))
+    palette = sns.color_palette("Set2")
+    sns.set_theme("notebook", "whitegrid", palette=palette, font="serif", font_scale=1.75)
+
+    sns.lineplot(
+        data=df_plot,
+        x=x_name,
+        y=y_name,
+        hue=hue,
+        style=style,
+        markers=True,
+        markersize=10,  # Increased marker size
+        linewidth=2,
+        errorbar='sd',
+        palette=palette
+    )
+
+    plt.xscale('log')
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+
+    # Add vertical lines
+    for xpos in [16, 256, 2000]:
+        plt.axvline(x=xpos, color='gray', linestyle='--', linewidth=2)
+
+    max_x = df_plot[x_name].max()
+    df_last = df_plot[df_plot[x_name] == max_x].copy()
+    df_last = df_last.groupby(hue)[y_name].mean().sort_values(ascending=False)
+    order = df_last.index.tolist()
+
+    handles, labels = plt.gca().get_legend_handles_labels()
+    label_to_handle = dict(zip(labels, handles))
+
+    new_labels = [f"{label.replace('KL Average - ', '')}" for label in labels if label != '']
+    label_map = dict(zip(labels, new_labels))
+
+    # Filter and order handles/labels based on sorted order
+    ordered_handles = [label_to_handle[label] for label in order]
+    ordered_clean_labels = [label_map[label] for label in order]
+    print(ordered_handles)
+    print(ordered_clean_labels)
+
+    plt.legend(handles=ordered_handles, labels=ordered_clean_labels, loc='upper left', bbox_to_anchor=(0, 1))
+    plt.tight_layout()
+    plt.savefig(save_loc, bbox_inches='tight')
+    plt.close()
