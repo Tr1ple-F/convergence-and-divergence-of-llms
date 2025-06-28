Fixed unigram calculation

pile_sampler_frequency_bert.py
@@ -15,7 +15,7 @@ tokenizer = AutoTokenizer.from_pretrained(
     add_eos_token=True
 )
 
-token_counter = np.ndarray(len(tokenizer))
+token_counter = np.zeros(len(tokenizer))
 
 i = 0
 for x in range(len(ds)):

unigram_bert.py
@@ -1,7 +1,9 @@
+import ipdb
 import numpy as np
 
 frequency_count = np.load("frequency_count_bert.npy")
-unigram_distribution = np.log(frequency_count / np.sum(frequency_count))
+ipdb.set_trace()
+unigram_distribution = np.log((frequency_count+1) / np.sum(frequency_count))
 uniform_distribution = np.asarray([np.log(1 / len(frequency_count))] * 30522)
 
 np.save("unigram_dist_bert.npy", unigram_distribution)