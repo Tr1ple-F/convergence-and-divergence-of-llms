31cf5a43a1ee6c4c777a38ca676cf510918c46b5
Tr1ple-F <32393151+Tr1ple-F@users.noreply.github.com>
2025-06-30 19:03:28 +0200

    Fixed script for server

--- execute_bert.sh
+++ execute_bert.sh
@@ -10,9 +10,9 @@ fi
 WORKING_DIR="$1"
 
 # Step 3: Run python runner_seeds.py inside ./run-stage
-python runner_bert.py "$WORKING_DIR" 0,1,2,3,4 || { echo "Failed to run runner_seeds.py"; exit 1; }
+srun --gpus=1 --partition standard --unbuffered ../../venv/bin/python runner_bert.py "$WORKING_DIR" 0,1,2,3,4 || { echo "Failed to run runner_seeds.py"; exit 1; }
 
 # Step 4: Run python seeds_kl_torch.py and seeds_stats.py in ./stats-stage
 cd ../stats-stage || { echo "Directory ./stats-stage not found"; exit 1; }
-python bert_stats.py "$WORKING_DIR" || { echo "Failed to run seeds_stats.py"; exit 1; }
-python bert_uni.py "$WORKING_DIR" || { echo "Failed to run seeds_stats.py"; exit 1; }
+srun --gpus=1 --partition standard --unbuffered ../../venv/bin/python bert_stats.py "$WORKING_DIR" || { echo "Failed to run seeds_stats.py"; exit 1; }
+srun --gpus=1 --partition standard --unbuffered ../../venv/bin/python bert_uni.py "$WORKING_DIR" || { echo "Failed to run seeds_stats.py"; exit 1; }
