c6c3e7ba9a2983a9eb50068cac8d56ac0ef4c354
Tr1ple-F <32393151+Tr1ple-F@users.noreply.github.com>
2025-07-01 13:31:57 +0200

    Small changes for ICL plotting

--- paper-plots/icl_plotter.py
+++ paper-plots/icl_plotter.py
@@ -2,8 +2,10 @@ import sys
 import pandas as pd
 from utils import styled_plot
 
-path = f'../working_dir/{sys.argv[1]}/output'
+path = f'../working_dir/{sys.argv[1]}'
 df = pd.read_csv(f'{path}/seeds_icl.csv')
+df['Training Step'] = df['Training Step'].str.replace('step', '', regex=False).astype(int)
+df['Model'] = df['Model'].str.replace('EleutherAI/pythia-', '', regex=False).astype(str)
 
 styled_plot(
     df,
